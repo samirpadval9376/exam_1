95ee0adf6410a5cebeb21b5a1acd2f97be7e3fe6
MAYANK GHOGHARI <Ghogharimayank360@gmail.com>
2023-09-04 18:04:26 +0530

    MG  ||

---
+++ lib/views/screens/splesh_screen.dart
@@ -0,0 +1,36 @@
+import 'package:exam_1/views/screens/home_page.dart';
+import 'package:flutter/material.dart';
+import 'package:animated_splash_screen/animated_splash_screen.dart';
+
+class splesh extends StatelessWidget {
+  const splesh({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: AnimatedSplashScreen(
+          splash: Transform.scale(
+            scale: 4,
+            child: Container(
+              height: 500,
+              width: 90,
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: Colors.black,
+                  width: 2,
+                ),
+                image: const DecorationImage(
+                    image: NetworkImage(
+                        "https://cdn.pixabay.com/photo/2020/10/27/12/50/cat-5690627_1280.png"),
+                    fit: BoxFit.fill),
+              ),
+              child: const CircularProgressIndicator(),
+            ),
+          ),
+          nextScreen: const HomePage(),
+        ),
+      ),
+    );
+  }
+}
